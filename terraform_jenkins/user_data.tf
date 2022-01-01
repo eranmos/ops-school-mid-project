ee65b3aaa49c867d9310670a3c01ce490ccd2a76
= <=>
2022-01-01 16:30:32 +0200

    updating user data files for Jenkins slave & master

--- terraform_jenkins/user_data.tf
+++ terraform_jenkins/user_data.tf
@@ -11,6 +11,9 @@ sudo apt install awscli -y
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDe3eJDO2vAennWX4IDtRAU3XPHr0LGXQaKyKhbgD98woWkHp7Fy0mnxiTeN4aVZ5EuBWh5Ganyvud0giRLucVDjH/mvkhrhuCGmTnOLVswsF15O2TYlBlOO5jtSvfSqmjy1cvytQ2SQZAP+WbmasOmAs5+r45PfWFcSSD/ZOnEE0KcgB/vbdCbxQQq+FKfZnI4t5bFdMm6ZJ4SlI+MioME6jPFKb6n4qq2TJrvTbgrJDyEA+EEMX2v/3BtfS+fXP3LIvJIED1NNEMC3YNQQydWxQzz1XG48A/G7pP/1VSliP65YYHFbq9SXPezmCy1aHS9JbX+jy7JN+S1ksyVEZZ6n/2dDbedg7YuoSWn/mc9d6nmpcbAYQ+TaruiABiHdDzBx+XLk7uwSTnUli1TTaHgStohGV3vw8iqNvp7khnvnUc1dh3yU+c73oNpCpToufwAu2yLHMg1s5gzCZs1PqWqjOgWySdNEAG9NtHZvTHHh3QPcm82S2ZRvxBjr3v5dlM5qbP3+bNM6kc9ylCjygcJRb2dVkHlBBL4Avv0Dq+36iy8Tk4uKrKmdJtXvBertuTessknyb2lRzjNjyRRwSwaTHEdIUNOiZTt3ZZyRAm6h0vxFKixfERDpnCznH4DtLTwdA5HtWe+5uK/2fXWCu6c5pbLz4Zv5os1BuuJep1vRQ== ubuntu@ip-172-31-22-246" >> /home/ubuntu/.ssh/authorized_keys
 chmod  600 /home/ubuntu/.ssh/authorized_keys
 sudo docker run -d --restart=always -p 8080:8080 -p 50000:50000 -v ${local.jenkins_home_mount} -v ${local.docker_sock_mount} --env ${local.java_opts} jenkins/jenkins"
+sudo sed -i $'/ExecStart=/c\\ExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock\n' /lib/systemd/system/docker.service
+sudo systemctl daemon-reload
+sudo service docker restart
 
 USERDATA
 }
@@ -37,6 +40,9 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ubuntu
 sudo usermod -aG docker jenkins
+sudo sed -i $'/ExecStart=/c\\ExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock\n' /lib/systemd/system/docker.service
+sudo systemctl daemon-reload
+sudo service docker restart
 
 # Ansible Server related
 sudo apt install python-pip -y
